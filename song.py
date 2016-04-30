0ac3b38ac75611365a818c9b059eeffee79ed94c
ack8 <andrewcklingelhofer@gmail.com>
2016-04-30 01:17:39 -0400

    Added all major keys

--- song.py
+++ song.py
@@ -3,12 +3,32 @@ from random import randint
 # Scales
 cMajor = ["C", "D", "E", "F", "G", "A", "B"]
 gMajor = ["G", "A", "B", "C", "D", "E", "F#"]
+dMajor = ["D", "E", "F#", "G", "A", "B", "C#"]
+aMajor = ["A", "B", "C#", "D", "E", "F#", "G#"]
+eMajor = ["E", "F#", "G#", "A", "B", "C#", "D#"]
+bMajor = ["B", "C#", "D#", "E", "F#", "G#", "A#"]
+fMajor = ["F", "G", "A", "Bb", "C", "D", "E"]
+bbMajor = ["Bb", "C", "D", "Eb", "F", "G", "A"]
+ebMajor = ["Eb", "F", "G", "Ab", "Bb", "C", "D"]
+abMajor = ["Ab", "Bb", "C", "Db", "Eb", "F", "G"]
+dbMajor = ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"]
+gbMajor = ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"]
 
 # Scale Options
 
 scales = {
     ('c', 'M'): cMajor,
     ('g', 'M'): gMajor,
+    ('d', 'M'): dMajor,
+    ('a', 'M'): aMajor,
+    ('e', 'M'): eMajor,
+    ('b', 'M'): bMajor,
+    ('f', 'M'): fMajor,
+    ('bb', 'M'): bbMajor,
+    ('eb', 'M'): ebMajor,
+    ('ab', 'M'): abMajor,
+    ('db', 'M'): dbMajor,
+    ('gb', 'M'): gbMajor,
 }
 
 # Song Options
@@ -31,6 +51,8 @@ def check_scale_options(i, song):
 
 def get_scale(key, m):
     return scales[(key, m)]    
+
+# Song Class
 
 class Song:
     
@@ -55,7 +77,7 @@ class Song:
         print "Random 8 Notes: " + str(sixteenNotes)
 
 def create_song():
-    print "Input Key: (c, g)"
+    print "Input Key: (c, g, d, a, e, b, f, bb, eb, ab, db, gb)"
     key = raw_input('>>> ')
     print "Major or Minor? (M or m)"
     m_m = raw_input('>>> ')
@@ -64,9 +86,11 @@ def create_song():
     song.printScale()
     return song
 
+# Song Loop
+
 def song_options(song):
     while True:
-        print "What would you like to do with this song?"
+        print "What would you like to do with this song? ('h' for help)"
         i = raw_input('>>> ')
         if i == 'e':
             break
