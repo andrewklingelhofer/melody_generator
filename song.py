91509dca3631cd7c8eb961f7d276c97cc4998863
ack8 <andrewcklingelhofer@gmail.com>
2016-04-30 16:39:25 -0400

    Added get_mode ability based on song scale

--- song.py
+++ song.py
@@ -1,5 +1,34 @@
 from random import randint
 
+# Song Options
+options = (
+    "'scale': print scale",
+    "'8notes': print 8 random notes",
+    "'16notes': print 16 random notes",
+    "'get mode <mode>': get mode from song key",
+    "'h': help",
+    "'e': exit song",
+)
+
+def check_scale_options(i, song):
+    if i == 'scale':
+        song.printScale()
+    elif i == '8notes':
+        song.random8Notes()
+    elif i == '16notes':
+        song.random16Notes()
+    elif i[:8] == 'get mode':
+        a = i.split(' ')
+        if len(a) > 2:
+            mode = a[2]
+            new_scale = get_mode(mode, song.scale)
+            print new_scale
+        else:
+            print "Please input mode name"
+    elif i == 'h':
+        for option in options:
+            print option
+
 # Major Scales
 cMajor = ["C", "D", "E", "F", "G", "A", "B"]
 gMajor = ["G", "A", "B", "C", "D", "E", "F#"]
@@ -28,8 +57,44 @@ fMinor = ["F", "G", "Ab", "Bb", "C", "Db", "Eb"]
 bbMinor = ["Bb", "C", "Db", "Eb", "F", "Gb", "Ab"]
 ebMinor = ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db"]
 
-# Scale Options
+# Major Chordal Relationships
+def get_mode(mode, scale):
+    # i.e. get_mode(lydian, cMajor): [F, G, A, B, C, D, E]
+    if mode == "ionian":
+        return scale
+    elif mode == "dorian":
+        dorian = scale[1:]
+        dorian.append(scale[0])
+        return dorian
+    elif mode == "phrygian":
+        phrygian = scale[2:]
+        for note in scale[:2]:
+            phrygian.append(note)
+        return phrygian
+    elif mode == "lydian":
+        lydian = scale[3:]
+        for note in scale[:3]:
+            lydian.append(note)
+        return lydian
+    elif mode == "mixolydian" or mode == "dominant":
+        dominant = scale[4:]
+        for note in scale[:4]:
+            dominant.append(note)
+        return dominant
+    elif mode == "aeolian" or mode == "minor":
+        aeolian = scale[5:]
+        for note in scale[:5]:
+            aeolian.append(note)
+        return aeolian
+    elif mode == "locrian":
+        locrian = scale[6:]
+        for note in scale[:6]:
+            locrian.append(note)
+        return locrian
+    else:
+        return None
 
+# Scale Options
 scales = {
     # Major
     ('c', 'M'): cMajor,
@@ -42,8 +107,11 @@ scales = {
     ('bb', 'M'): bbMajor,
     ('eb', 'M'): ebMajor,
     ('ab', 'M'): abMajor,
+    ('g#', 'M'): abMajor,
     ('db', 'M'): dbMajor,
+    ('c#', 'M'): dbMajor,
     ('gb', 'M'): gbMajor,
+    ('f#', 'M'): gbMajor,
     
     #Minor
     ('a', 'm'): aMinor,
@@ -51,7 +119,9 @@ scales = {
     ('b', 'm'): bMinor,
     ('f#', 'm'): fsharpMinor,
     ('c#', 'm'): csharpMinor,
+    ('db', 'm'): csharpMinor,
     ('ab', 'm'): abMinor,
+    ('g#', 'm'): abMinor,
     ('d', 'm'): dMinor,
     ('g', 'm'): gMinor,
     ('c', 'm'): cMinor,
@@ -60,26 +130,6 @@ scales = {
     ('eb', 'm'): ebMinor,
 }
 
-# Song Options
-
-options = (
-    "'scale': print scale",
-    "'8notes': print 8 random notes",
-    "'16notes': print 16 random notes",
-    "'h': help",
-    "'e': exit song",
-)
-
-def check_scale_options(i, song):
-    if i == 'scale':
-        song.printScale()
-    elif i == '8notes':
-        song.random8Notes()
-    elif i == '16notes':
-        song.random16Notes()
-    elif i == 'h':
-        for option in options:
-            print option
 
 def get_scale(key, m):
     return scales[(key, m)]    
@@ -121,7 +171,6 @@ def create_song():
     return song
 
 # Song Loop
-
 def song_options(song):
     while True:
         print "What would you like to do with this song? ('h' for help)"
